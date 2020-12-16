1e48f0b1ea643249017205a8244c3699472cacbd
Mustafa Yencilek <52329440+MustafaYencilek@users.noreply.github.com>
2020-12-16 03:24:14 +0300

    Add files via upload

---
+++ Area_Perimeter.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class Lab01_20190808038 {
+    
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int LengthOfRectangle;
+        int WidthOfRectangle;
+        
+        
+        System.out.println("Please write the length of the  rectangle that we will calculate");
+        
+        LengthOfRectangle=scanner.nextInt();
+        
+        System.out.println("Please write the width of the  rectangle that we will calculate");
+        
+        WidthOfRectangle=scanner.nextInt();
+        
+        System.out.println("calculating results");
+        
+        System.out.println("*************************************");
+        
+        System.out.println("area of the rectangle: "+(LengthOfRectangle*WidthOfRectangle));
+        
+        System.out.println("*************************************");
+        
+        System.out.println("perimeter of the rectangle: "+(LengthOfRectangle+WidthOfRectangle)*2);
+        
+        
+        
+        System.out.println("\n \n ********program are completed******** ");
+        
+    }
+}
